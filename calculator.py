2ddcba7a2e468d0b21a16a302cb15aa13f064703
Julia Jones <julia@Julias-MacBook-Pro.local>
2025-11-14 13:18:19 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -8,12 +8,7 @@ One function per operation, in order.
 
 import math
 
-def add(a, b): 
-    return a + b
-
-import math
-
-def sqaure_root(a):
+def square_root(a):
     try:
         if a < 0:
             raise ValueError
@@ -21,12 +16,11 @@ def sqaure_root(a):
     except ValueError as e:
         raise e
 
-def hypoenuse(a, b):
+def hypotenuse(a, b):
     try:
         return math.hypot(a, b)
     except Exception as e:
         raise e
-
 
 def add(a,b):
     return a+b
@@ -48,17 +42,3 @@ def logarithm(a,b):
 
 def exp(a,b):
     return a**b
-
-
-def subtract(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def logarithm(a, b):
-    if a<= 0 or b <= 0 or b == 1:
-        raise ZeroDivisionError
-
-def exp(a,b):
-    return a ** b
